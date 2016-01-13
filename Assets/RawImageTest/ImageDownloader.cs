b4655ac8befa1fd3d0e6021767c3a46626462c06
tshiraishi <tshiraishi@mushikago.com>
2016-01-13 10:26:53 +0900

    1st commit

---
+++ Assets/RawImageTest/ImageDownloader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.IO;
+
+public class ImageDownloader : MonoBehaviour
+{
+	IEnumerator Start()
+	{
+		RawImage rawImage = GetComponent<RawImage>();
+
+		Debug.Log (Application.persistentDataPath + "/app.png");
+
+		byte[] bytes = File.ReadAllBytes(Application.persistentDataPath + "/app.png");
+		Texture2D texture = new Texture2D(200, 200);
+		texture.filterMode = FilterMode.Trilinear;
+		texture.LoadImage(bytes);
+
+		rawImage.texture = texture;
+		rawImage.SetNativeSize();
+
+		yield return null;
+	}
+}
